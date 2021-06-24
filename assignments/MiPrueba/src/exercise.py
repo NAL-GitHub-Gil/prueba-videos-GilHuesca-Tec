96a6c3d10d4932c8e1db416c655c476578b93d05
GilHuesca-Tec <85255575+GilHuesca-Tec@users.noreply.github.com>
2021-06-24 15:59:36 -0500

    Update exercise.py
    Segunda entrega.

--- assignments/MiPrueba/src/exercise.py
+++ assignments/MiPrueba/src/exercise.py
@@ -1,8 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    a = int(input("Dame un número: ")
-    b = int(input("Dame un número: ")
-    c = int(input("Dame un número: ")
+    a = int(input("Dame un número: "))
+    b = int(input("Dame un número: "))
+    c = int(input("Dame un número: "))
     sum = a + b + c
     print("La suma es " + str(sum))
     pass
